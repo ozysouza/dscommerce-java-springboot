bb79ae73bf2980e96bd84f56bcc59fa77d5f4e93
ozysouza <ozysouzaa@gmail.com>
2025-05-29 00:37:35 -0400

    Create and Map Category table

---
+++ src/main/java/com/desouza/dscommerce/entities/Category.java
@@ -0,0 +1,47 @@
+package com.desouza.dscommerce.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private Set<Product> products = new HashSet<>();
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+}
