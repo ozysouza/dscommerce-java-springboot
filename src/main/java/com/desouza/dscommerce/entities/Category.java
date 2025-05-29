a22f255fc304f0b7247503bff42be6fa33cc39c6
ozysouza <ozysouzaa@gmail.com>
2025-05-29 00:49:27 -0400

    Create association between Product and Category table, join it on new table

--- src/main/java/com/desouza/dscommerce/entities/Category.java
+++ src/main/java/com/desouza/dscommerce/entities/Category.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +19,7 @@ public class Category {
     private Long id;
     private String name;
 
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
