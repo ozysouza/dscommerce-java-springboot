2775f16de37ae6da5234f20f6eb71894c454034a
ozysouza <ozysouzaa@gmail.com>
2025-06-01 23:27:25 -0400

    Add pagination when returning all products

--- src/main/java/com/desouza/dscommerce/controllers/ProductController.java
+++ src/main/java/com/desouza/dscommerce/controllers/ProductController.java
@@ -1,8 +1,8 @@
 package com.desouza.dscommerce.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +19,8 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping
-    public List<ProductDTO> findAll() {
-        return service.findAll();
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 
     @GetMapping(value = "/{id}")
--- src/main/java/com/desouza/dscommerce/service/ProductService.java
+++ src/main/java/com/desouza/dscommerce/service/ProductService.java
@@ -1,8 +1,8 @@
 package com.desouza.dscommerce.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,9 +19,9 @@ public class ProductService {
     private ProductRepository repository;
 
     @Transactional(readOnly = true)
-    public List<ProductDTO> findAll() {
-        List<Product> result = repository.findAll();
-        return result.stream().map(x -> new ProductDTO(x)).toList();
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 
     @Transactional(readOnly = true)
