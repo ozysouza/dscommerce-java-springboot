869fe7419ed8e160aea1127f36c1389e673d6a19
ozysouza <ozysouzaa@gmail.com>
2025-06-01 22:48:37 -0400

    Create PoructService, return productByID from the repository

---
+++ src/main/java/com/desouza/dscommerce/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.desouza.dscommerce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.desouza.dscommerce.dto.ProductDTO;
+import com.desouza.dscommerce.entities.Product;
+import com.desouza.dscommerce.repositories.ProductRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Product product = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Product with ID " + id + " not found"));
+        return new ProductDTO(product);
+    }
+
+}
