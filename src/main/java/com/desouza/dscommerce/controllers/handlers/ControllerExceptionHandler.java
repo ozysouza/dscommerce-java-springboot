a514545b136455d40e40fdd00329e8eb8399d7a6
ozysouza <ozysouzaa@gmail.com>
2025-06-03 00:09:16 -0400

    Create custom validation for error field messages

--- src/main/java/com/desouza/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/desouza/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -4,10 +4,13 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.desouza.dscommerce.dto.CustomError;
+import com.desouza.dscommerce.dto.ValidationError;
 import com.desouza.dscommerce.service.exceptions.DataBaseException;
 import com.desouza.dscommerce.service.exceptions.ResourceNotFoundException;
 
@@ -27,6 +30,19 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> dataBase(DataBaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(e.getMessage(), status.value(), request.getRequestURI(), Instant.now());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e,
+            HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError("Invalid data", status.value(), request.getRequestURI(),
+                Instant.now());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
---
+++ src/main/java/com/desouza/dscommerce/dto/ValidationError.java
@@ -0,0 +1,23 @@
+package com.desouza.dscommerce.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError {
+
+    private List<FieldMessage> erros = new ArrayList<>();
+
+    public ValidationError(String error, Integer status, String path, Instant timestamp) {
+        super(error, status, path, timestamp);
+    }
+
+    public List<FieldMessage> getErros() {
+        return erros;
+    }
+
+    public void addError(String fieldName, String message) {
+        erros.add(new FieldMessage(fieldName, message));
+    }
+
+}
