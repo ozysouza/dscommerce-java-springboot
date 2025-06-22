10ac77bc6c12a126b484fd9e9c4cf0dcedeafa4a
ozysouza <ozysouzaa@gmail.com>
2025-06-22 13:29:11 -0400

    Create UserService, implements UserDetailsService and peform loadUser function

---
+++ src/main/java/com/desouza/dscommerce/service/UserService.java
@@ -0,0 +1,39 @@
+package com.desouza.dscommerce.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.desouza.dscommerce.entities.Role;
+import com.desouza.dscommerce.entities.User;
+import com.desouza.dscommerce.projections.UserDetailsProjection;
+import com.desouza.dscommerce.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+
+}
