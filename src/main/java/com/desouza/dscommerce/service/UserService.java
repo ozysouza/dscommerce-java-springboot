f6e939add031004c01296770d9d942af324fcc4a
ozysouza <ozysouzaa@gmail.com>
2025-06-22 18:40:41 -0400

    Created protected function to return logged user

--- src/main/java/com/desouza/dscommerce/service/UserService.java
+++ src/main/java/com/desouza/dscommerce/service/UserService.java
@@ -3,9 +3,12 @@ package com.desouza.dscommerce.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import com.desouza.dscommerce.entities.Role;
@@ -34,6 +37,19 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    @Transactional(readOnly = true)
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+
+            return userRepository.findByEmail(username).get();
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("User not found");
+        }
     }
 
 }
