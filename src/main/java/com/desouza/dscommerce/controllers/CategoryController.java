1f664fb8ce6f6ec2daf1663cfeb03066ebafb31b
ozysouza <ozysouzaa@gmail.com>
2025-06-24 23:23:00 -0400

    Create Service and Controller to return all caregories

---
+++ src/main/java/com/desouza/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,27 @@
+package com.desouza.dscommerce.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desouza.dscommerce.dto.category.CategoryDTO;
+import com.desouza.dscommerce.service.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> categoryList = categoryService.findAll();
+        return ResponseEntity.ok(categoryList);
+    }
+
+}
---
+++ src/main/java/com/desouza/dscommerce/service/CategoryService.java
@@ -0,0 +1,25 @@
+package com.desouza.dscommerce.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.desouza.dscommerce.dto.category.CategoryDTO;
+import com.desouza.dscommerce.entities.Category;
+import com.desouza.dscommerce.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> result = categoryRepository.findAll();
+        return result.stream().map(x -> new CategoryDTO(x)).toList();
+    }
+
+}
