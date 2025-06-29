50629f7a31f98e5acd3f1791f9c6a352d3fc4395
ozysouza <ozysouzaa@gmail.com>
2025-06-29 15:47:39 -0400

    Replace PageRequest for Pageable to keep same pattern as in Product

--- src/main/java/com/desouza/dscommerce/service/CategoryService.java
+++ src/main/java/com/desouza/dscommerce/service/CategoryService.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,8 +37,8 @@ public class CategoryService {
     }
 
     @Transactional(readOnly = true)
-    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
-        Page<Category> categories = categoryRepository.findAll(pageRequest);
+    public Page<CategoryDTO> findAllPaged(Pageable pageable) {
+        Page<Category> categories = categoryRepository.findAll(pageable);
         return categories.map(x -> new CategoryDTO(x));
     }
 
