617c37e01e604896ab3c9bfcf7e66eb1df42dde0
ozysouza <ozysouzaa@gmail.com>
2025-07-02 23:56:57 -0400

    Create ProductFactory

---
+++ src/test/java/com/desouza/dscommerce/tests/ProductFactory.java
@@ -0,0 +1,20 @@
+package com.desouza.dscommerce.tests;
+
+import com.desouza.dscommerce.dto.product.ProductDTO;
+import com.desouza.dscommerce.entities.Category;
+import com.desouza.dscommerce.entities.Product;
+
+public class ProductFactory {
+
+    public static Product createProduct() {
+        Product product = new Product(null, "Phone", "Good Phone", 800.0, "https://img.com/img.png");
+        product.getCategories().add(new Category(2L, "Electronic"));
+        return product;
+    }
+
+    public static ProductDTO createProductDTO() {
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+
+}
