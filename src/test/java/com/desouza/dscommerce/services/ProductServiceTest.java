0685fddd3aa1652d6f986fc63bea72657875aba0
ozysouza <ozysouzaa@gmail.com>
2025-07-03 23:56:53 -0400

    Add Mockito to fake repository calls on searchProductsCategories, save and searchById.

--- src/test/java/com/desouza/dscommerce/services/ProductServiceTest.java
+++ src/test/java/com/desouza/dscommerce/services/ProductServiceTest.java
@@ -1,18 +1,25 @@
 package com.desouza.dscommerce.services;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.desouza.dscommerce.entities.Product;
 import com.desouza.dscommerce.repositories.ProductRepository;
 import com.desouza.dscommerce.service.ProductService;
 import com.desouza.dscommerce.service.exceptions.DataBaseException;
 import com.desouza.dscommerce.service.exceptions.ResourceNotFoundException;
+import com.desouza.dscommerce.tests.ProductFactory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTest {
@@ -26,12 +33,16 @@ public class ProductServiceTest {
     private Long validId;
     private Long invalidId;
     private Long associatedId;
+    private PageImpl<Product> page;
+    private Product product;
 
     @BeforeEach
     void setUp() throws Exception {
         validId = 1L;
         invalidId = 2L;
         associatedId = 3L;
+        product = ProductFactory.createProduct();
+        page = new PageImpl<>(List.of(product));
 
         Mockito.doNothing().when(productRepository).deleteById(validId);
         Mockito.doThrow(ResourceNotFoundException.class).when(productRepository).deleteById(invalidId);
@@ -40,6 +51,14 @@ public class ProductServiceTest {
         Mockito.when(productRepository.existsById(validId)).thenReturn(true);
         Mockito.when(productRepository.existsById(invalidId)).thenReturn(false);
         Mockito.when(productRepository.existsById(associatedId)).thenReturn(true);
+
+        Mockito.when(productRepository.searchProductsCategories((Pageable) ArgumentMatchers.any(),
+                ArgumentMatchers.anyString())).thenReturn(page);
+
+        Mockito.when(productRepository.save(ArgumentMatchers.any())).thenReturn(product);
+
+        Mockito.when(productRepository.searchById(validId)).thenReturn(product);
+        Mockito.when(productRepository.searchById(invalidId)).thenReturn(null);
     }
 
     @Test
