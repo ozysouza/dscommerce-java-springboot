7f379bcebf6dae327d9e566a9dba72dcbb6c438d
ozysouza <ozysouzaa@gmail.com>
2025-07-05 00:40:41 -0400

    Create categorty factory and updates usages

--- src/test/java/com/desouza/dscommerce/tests/ProductFactory.java
+++ src/test/java/com/desouza/dscommerce/tests/ProductFactory.java
@@ -7,14 +7,18 @@ import com.desouza.dscommerce.entities.Product;
 public class ProductFactory {
 
     public static Product createProduct() {
-        Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png");
-        product.getCategories().add(new Category(2L, "Electronic"));
+        Product product = new Product(1L, "iPhone 15", "Super Retina XDR display; 6.1‑inch (diagonal)", 999.99, "https://img.com/img.png");
+        product.getCategories().add(createCategory());
         return product;
     }
 
     public static ProductDTO createProductDTO() {
         Product product = createProduct();
         return new ProductDTO(product, product.getCategories());
+    }
+
+    public static Category createCategory() {
+        return new Category(1L, "Electronic");
     }
 
 }
