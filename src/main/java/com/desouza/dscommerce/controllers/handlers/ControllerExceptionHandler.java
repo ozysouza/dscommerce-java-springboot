01d935336705dad613b3afcd982c370156673dc8
ozysouza <ozysouzaa@gmail.com>
2025-07-16 22:57:56 -0400

    Add PropertyReferenceException validation

--- src/main/java/com/desouza/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/desouza/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.desouza.dscommerce.controllers.handlers;
 
 import java.time.Instant;
 
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -44,6 +45,13 @@ public class ControllerExceptionHandler {
         for (FieldError f : e.getBindingResult().getFieldErrors()) {
             err.addError(f.getField(), f.getDefaultMessage());
         }
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ResponseEntity<CustomError> propertyReference(PropertyReferenceException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(e.getMessage(), status.value(), request.getRequestURI(), Instant.now());
         return ResponseEntity.status(status).body(err);
     }
 
