c8ae2a0279d7e850d924ce32b3f60d55e877634c
ozysouza <ozysouzaa@gmail.com>
2025-08-18 23:17:36 -0400

    Create email service, exception and update pom.xml

---
+++ src/main/java/com/desouza/dscommerce/service/exceptions/EmailException.java
@@ -0,0 +1,8 @@
+package com.desouza.dscommerce.service.exceptions;
+
+public class EmailException extends RuntimeException {
+
+    public EmailException(String msg) {
+        super(msg);
+    }
+}
